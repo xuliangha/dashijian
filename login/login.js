5b07714ed710f851e6d005894a4143a507597fc8
Your Name <you@example.com>
2021-02-27 11:24:29 +0800

    完成了注册的验证

--- login/login.js
+++ login/login.js
@@ -1,5 +1,7 @@
 //使用的layui
 let layer = layui.layer;
+let form = layui.form;
+
 // 切换登录和注册
 $('#goto-register').on('click', function () {
   $('#login').hide()
@@ -11,6 +13,38 @@ $('#goto-login').on('click', function () {
 })
 
 
+
+//注册密码的验证
+form.verify({
+  changdu: function(value, dom) {
+    let reg = /^\S{6,12}$/;
+    if (reg.test(value) == false) {
+      return "密码必须6到12位，且不能出现空格";
+    }
+  },
+  same: function(value, dom) {
+    // value：谁的value？重复密码的值
+    // 验证：获取密码值！密码的值 如何获取?  设置类名.val(); 
+    //       可以不写正则！
+    if ($(".pwd").val() != value) {
+      return "密码和重复密码必须一致!"
+    }
+  },
+});     
+
+
+
+
+
+
+
+
+
+
+
+
+
+//注册密码
 $('.layui-form').on('submit', function (e) {
   e.preventDefault()
   let data = $(this).serialize()
